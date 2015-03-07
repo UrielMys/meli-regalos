ad239ab5b5731fe559b6ffbe8c38a513d9c7323f
jorgecabrera <jorge.cabrera@mercadolibre.com>
2015-03-07 13:20:53 -0300

    Cambios en enviar mail controller deje de usar el servicio que habia declarado. TODO : eliminar servicio.

--- grails-app/controllers/meli/regalos/EnviarMailController.groovy
+++ grails-app/controllers/meli/regalos/EnviarMailController.groovy
@@ -2,16 +2,22 @@ package meli.regalos
 
 class EnviarMailController {
 
-	def testService;
+	def mailService;
 	def index() {
+		enviarMail;
 	}
-	def enviarMail(){
+	def enviarMail = {
 		/*Date fechaActual = new Date();
-		def regalosDelMes = Regalo.findAll();
-		regalosDelMes = regalosDelMes.findAll{it.fechaEntrega.getAt(Calendar.MONTH)==fechaActual.getAt(Calendar.MONTH)}
-		println regalosDelMes;
-		[regalos:regalosDelMes];*/
-		testService.sendConfirmation()
+		 def regalosDelMes = Regalo.findAll();
+		 regalosDelMes = regalosDelMes.findAll{it.fechaEntrega.getAt(Calendar.MONTH)==fechaActual.getAt(Calendar.MONTH)}
+		 println regalosDelMes;
+		 [regalos:regalosDelMes];*/
+		mailService.sendMail{
+			to "jorgejcabrera@hotmail.com.ar"
+			from "urieljorgemysler@hotmail.com"
+			subject "Hello John"
+			text "probando mails"
+		}
 		render "OK"
 	}
 }
